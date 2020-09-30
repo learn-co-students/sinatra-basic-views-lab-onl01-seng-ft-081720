cf2a10e4dd628eadc5eb51fc27ca2fe07b5c8b2b
mmccarthy64 <mmccarthy64@hotmail.com>
2020-09-30 09:49:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
 
+    # get &x27;&x2F;&x27; do
+    #     &quot;Hello World&quot;
+    # end
 end
